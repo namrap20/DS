9ccbacf718af7266205265fbccfd1c67d8c8fc9f
namrap20 <namrap2008@gmail.com>
2026-07-23 11:57:05 +0530

    Unit-1
    Unit-1

---
+++ unit 1/program 1.c
@@ -0,0 +1,36 @@
+//create an arrayof size 10, input values,print the array,and search an element.
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a[10],i,search,found=0;
+    printf("enter 10 elements:\n");
+    for (i=0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("\n array elements are:\n ");
+    for(i=0;i<10;i++)
+     {
+        printf("%d ", a[i]);
+    }
+
+    printf("\n\nEnter element to search: ");
+    scanf("%d", &search);
+
+    for(i=0; i<10; i++)
+    {
+        if(a[i] == search)
+        {
+            found = 1;
+            printf("Element found at position %d", i + 1);
+            break;
+        }
+    }
+
+    if(found == 0)
+        printf("Element not found.");
+
+    getch();
+}
+
